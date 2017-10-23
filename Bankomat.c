73b940de71004c50ab6c720f6eea336ba11412e6
KonstantinovaKS <katia1234konsta@gmail.com>
2017-10-23 10:08:32 +0300

    changet bankomat

--- Bankomat.c
+++ Bankomat.c
@@ -1,26 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
+#define ONE 1
+#define TWO 2
+#define THREE 3
 
 int main()
 {
 
+int const THOUSAND = 1000;
+int const FIFTY = 5000;
 
 int isAdmin=0;
 int isLoggedIn=0;
+int pinAdmin=55555;
+
+int pinCodArray[5000];
+int ballance[5000];
+int n = 0 ;
+int j = 0;
 int ball=0;
 int cash=0;
-int pinkodArray[5000];
-int ballance[5000];
-int n;
-int j;
-int pinAdmin=55555;
-int i;
-int b;
+int i = 0;
+int b = 0;
 int randPin = 0;
 int pinCode = 0;
-int lenghtArray = 5000;
+int lenghtArray = FIFTY;
 
  srand(time(NULL));
 
@@ -28,7 +33,7 @@ int lenghtArray = 5000;
 /*/initializatin  pinkod*/
     for ( i = 0; i < lenghtArray; i++)
     {
-        pinkodArray [i] = 0;
+        pinCodArray [i] = 0;
 
     }
 
@@ -36,8 +41,8 @@ int lenghtArray = 5000;
 
     for ( i = 0; i < lenghtArray; i++)
     {
-        randPin = rand()%5000;
-        pinkodArray[i] = 1000+ randPin;
+        randPin = rand()% FIFTY;
+        pinCodArray[i] = THOUSAND+ randPin;
     }
 //initializatinn ballance
      for (i = 0; i< lenghtArray; i++ )
@@ -47,60 +52,58 @@ int lenghtArray = 5000;
 
      for(i = 0;i< lenghtArray; i++)
      {
-          randPin = rand()%5000;
-        ballance[i] = 1000+ randPin;
+        randPin = rand()% FIFTY;
+        ballance[i] = THOUSAND+ randPin;
      }
 
 // Enter pinkod
-   for (i=0; i< 3 ; i++)
-   {
+   for (i=0; i< 3 ; i++) {
 
-       if (isLoggedIn || isAdmin)
-       {
+
+       if (isLoggedIn || isAdmin) {
+
            break;
        }
         printf(" Enter pincode:\n");
         scanf("%d" , &pinCode);
-        for (j =0;j < lenghtArray ; j++ )
-        {
-            if (pinCode == pinkodArray[j])
-            {
+        for (j =0;j < lenghtArray ; j++ )   {
 
+            if (pinCode == pinCodArray[j]) {
                 printf("Hello user!\n");
-                isLoggedIn=1;
-                ball=j;
+                isLoggedIn = 1;
+                ball = j;
 
                 break;
-            }else if ( pinCode==pinAdmin){
+            }else if ( pinCode == pinAdmin){
                 printf("Hello Admin\n");
-                isAdmin=1;
+                isAdmin = 1;
                // isLoggedIn=1;
                 break;
 
             }
         }
-        if (i==3)
-        {
+        if (i == THREE) {
+
             return 0 ;
         }
    }
 
 // Menu for user
-        if (isLoggedIn==1)
-           {
+        if (isLoggedIn == 1) {
+
        	 printf("1: Balance\n");
        	 printf("2: withdraw money\n");
        	 printf("3: Put money\n");
        	 scanf("%d",&n);
        	 switch(n)
        	 {
-       	 case 1: printf("\n Balance %d", ball);
+       	 case ONE: printf("\n Balance %d", ball);
        	 break;
-       	 case 2: printf("\n Take cash: ");
-       	         scanf("%d" ,&cash);
-       	         printf("\nBallance = %d\n", ball- cash);
+       	 case TWO: printf("\n Take cash: ");
+                   scanf("%d" ,&cash);
+       	           printf("\nBallance = %d\n", ball- cash);
        	 break;
-       	 case 3: printf("\n Put cash \n",cash);
+       	 case THREE: printf("\n Put cash \n",cash);
        	          scanf("%d" ,&cash);
        	         printf("\nBallance = %d", ball+ cash);
        	 break;
@@ -108,7 +111,7 @@ int lenghtArray = 5000;
        	  }
             }
 // Menu for admin
-        if (isAdmin==1)
+        if (isAdmin == 1)
             {
          printf("1: Do you want to NULL?\n");
        	 printf("2: Look all  pins\n");
@@ -116,14 +119,14 @@ int lenghtArray = 5000;
        	 scanf("%d",&b);
        	 switch(b)
        	 {
-       	 case 1:
+       	 case ONE:
        	       for (i = 0; i< lenghtArray; i++ )
                {
          ballance [i] = 0;
          printf("User : %d\n", ballance[i]);
                }
          break;
-       	 case 2:
+       	 case TWO:
        	     for (i=0; i<lenghtArray; i++)
                 printf("User %d: %d\n", i+1, ballance[i]);
        	 break;
