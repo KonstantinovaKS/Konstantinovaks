9a1980c212e9384ba98a14eb1a955c11acf081aa
KonstantinovaKS <katia1234konsta@gmail.com>
2017-10-31 17:57:26 +0200

    Change

--- Homework_string.c
+++ Homework_string.c
@@ -1,21 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <malloc.h>
 #include<string.h>
 #include<ctype.h>
+#define THOUSAND 1000
+#define HUNDRED 100
 
 
 int main(){
 
-char arraySymbol[100];
-int counter[1000];
+char arraySymbol[HUNDRED];
+int counter[THOUSAND];
 char abc[] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
 
 int i;
 int j;
 
 //initialization counter 0
-for(i = 0 ; i < 1000; i ++){
+for(i = 0 ; i < THOUSAND; i ++){
     counter[i] = 0;
 }
 
@@ -28,23 +29,26 @@ for (i = 0 ; i< strlen(arraySymbol); i++){
 arraySymbol[i] = tolower (arraySymbol[i]);
 }
 
-//Search letter
+//Input letter
 for (i = 0; i < strlen(abc); i++){
    for (j = 0; j < strlen(arraySymbol); j++){
       if (arraySymbol[j] == abc[i]){
       counter[i]++;
-      }
-    }
-}
+         }
+       }
+     }
 //sort
 for (i = strlen(arraySymbol); i > 0; i--){
-     for( j= 0; j < 1000; j++){
-if (counter[j] == i){
-
-printf("%d  %c\n", counter[j] , abc[j]);
- }
+     for( j= 0; j < THOUSAND; j++){
+          if (counter[j] == i){
+          printf("%d  %c\n", counter[j] , abc[j]);
+            }
+         }
      }
-}
+
 return 0 ;
+
+
 }
+
             //  }
